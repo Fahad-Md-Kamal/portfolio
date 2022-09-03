773455b5cae2f9791e5a8dd47b11669c3f995e70
fahad-md-kamal <faahad.hossain@gmail.com>
2022-09-03 12:30:07 +0600

    Dockerized Portfolio App

--- conf/settings.py
+++ conf/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '' 
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['fahadresume.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['fahadresume.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -107,10 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
